1d3bab9706b830a8fc3e98b91929d0090467327b
Smogg21 <spazzaglia98@gmail.com>
2024-08-05 15:42:13 -0500

    Worked on navigation inside app

--- src/presentation/routes/StackNavigator.tsx
+++ src/presentation/routes/StackNavigator.tsx
@@ -1,8 +1,9 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screens/home/HomeScreen';
-import {ProductScreen} from '../screens/products/ProductScreen';
 import {SettingsScreen} from '../screens/settings/SettingsScreen';
 import React from 'react';
+import {ProductScreen} from '../screens/products/ProductScreen';
+import {ProductsScreen} from '../screens/products/ProductsScreen';
 
 const Stack = createStackNavigator();
 
@@ -17,7 +18,8 @@ export const StackNavigator = () => {
         },
       }}>
       <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Products" component={ProductScreen} />
+      <Stack.Screen name="Products" component={ProductsScreen} />
+      <Stack.Screen name="Product" component={ProductScreen} />
       <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
   );
--- src/presentation/screens/products/ProductsScreen.tsx
+++ src/presentation/screens/products/ProductsScreen.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {globalStyles} from '../../theme/theme';
+import {FlatList} from 'react-native-gesture-handler';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {useNavigation} from '@react-navigation/native';
 
-export const ProductScreens = () => {
+const products = [
+  {id: 1, name: 'Producto1'},
+  {id: 2, name: 'Producto2'},
+  {id: 3, name: 'Producto3'},
+  {id: 4, name: 'Producto4'},
+  {id: 5, name: 'Producto5'},
+  {id: 6, name: 'Producto6'},
+];
+
+export const ProductsScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>ProductScreens</Text>
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom: 10, fontSize: 30}}>Productos</Text>
+      <FlatList
+        data={products}
+        renderItem={({item}) => (
+          <PrimaryButton
+            onPress={() => navigation.navigate('Product' as never)}
+            label={item.name}
+          />
+        )}
+      />
+      <Text style={{marginBottom: 10, fontSize: 30}}>Ajustes</Text>
+      <PrimaryButton
+        onPress={() => navigation.navigate('Settings' as never)}
+        label="Ajustes"
+      />
     </View>
   );
 };
