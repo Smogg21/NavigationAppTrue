9fa9d51087b05da03b8edd4f1a552765f1865e41
Smogg21 <spazzaglia98@gmail.com>
2024-08-06 19:15:14 -0500

    Worked on bottom bars

---
+++ src/presentation/components/shared/HamburgerMenu.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/no-unstable-nested-components */
+import {DrawerActions, useNavigation} from '@react-navigation/native';
+import React, {useEffect} from 'react';
+import {Pressable, Text} from 'react-native';
+
+export const HamburgerMenu = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerLeft: () => (
+        <Pressable
+          onPress={() => navigation.dispatch(DrawerActions.toggleDrawer)}>
+          <Text>Menú</Text>
+        </Pressable>
+      ),
+    });
+  }, []);
+
+  return <></>;
+};
